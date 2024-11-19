53389d5f17bea41ac60b0f1dda2017a450827b06
= <=>
2024-11-19 18:23:12 +0300

    feat: add stopwatch and moves counter

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ class MemoryGame {
     this.colorsPicklist = [...colors, ...colors];
     this.tileCount = this.colorsPicklist.length;
 
+    this.movesCounterElement = document.querySelector(".moves");
+    this.stopwatchElement = document.querySelector(".timer");
+
     this.revealedCount = 0;
+    this.movesCounter = 0;
+    this.stopwatch;
     this.activeTile = null;
     this.awaitingEndOfMove = false;
 
@@ -18,6 +23,8 @@ class MemoryGame {
       const tile = this.buildTile(color);
       this.container.appendChild(tile);
     });
+
+    this.startStopwatch();
   }
 
   handleTileClick(element, color) {
@@ -42,6 +49,7 @@ class MemoryGame {
     } else {
       this.hideTiles(element);
     }
+    this.updateCounter();
   }
 
   isMatch(tile) {
@@ -61,6 +69,7 @@ class MemoryGame {
 
   checkWinCondition() {
     if (this.revealedCount === this.tileCount) {
+      clearInterval(this.stopwatch);
       alert("You win!");
     }
   }
@@ -98,6 +107,33 @@ class MemoryGame {
 
     return element;
   }
+
+  updateCounter() {
+    this.movesCounter++;
+    this.movesCounterElement.innerText = `Moves: ${this.movesCounter}`;
+  }
+
+  startStopwatch() {
+    let seconds = 0;
+    let minutes = 0;
+    let hours = 0;
+
+    this.stopwatch = setInterval(() => {
+      seconds++;
+      if (seconds > 59) {
+        seconds = 0;
+        minutes++;
+      }
+      if (minutes > 59) {
+        minutes = 0;
+        seconds = 0;
+        hours++;
+      }
+      this.stopwatchElement.innerText = `${hours.toString().padStart(2, "0")}:
+      ${minutes.toString().padStart(2, "0")}:
+      ${seconds.toString().padStart(2, "0")}`;
+    }, 1000);
+  }
 }
 
 const tilesContainer = document.querySelector(".tiles");
@@ -113,107 +149,3 @@ const colors = [
 ];
 
 const game = new MemoryGame(tilesContainer, colors);
-
-// const tilesContainer = document.querySelector(".tiles");
-// const colors = [
-//   "#FF5733", // ярко-оранжевый
-//   "#33FF57", // зелёный лайм
-//   "#5733FF", // синий фиолетовый
-//   "#FF33A8", // розовый
-//   "#33FFF3", // бирюзовый
-//   "#FFD433", // жёлтый
-//   "#FF914D", // коралловый
-//   "#9153FF", // пурпурный
-// ];
-// const colorsPicklist = [...colors, ...colors];
-// const tileCount = colorsPicklist.length;
-
-// let revealedCount = 0;
-// let activeTile = null;
-// let awaitingEndOfMove = false;
-
-// function initGame() {
-//   const shuffledColors = shuffle(colorsPicklist);
-//   shuffledColors.forEach((color) => {
-//     const tile = buildTile(color);
-//     tilesContainer.appendChild(tile);
-//   });
-// }
-
-// initGame();
-
-// function handleTileClick(element, color) {
-//   if (
-//     awaitingEndOfMove ||
-//     element.getAttribute("data-revealed") === "true" ||
-//     element === activeTile
-//   ) {
-//     return;
-//   }
-
-//   element.style.backgroundColor = color;
-
-//   if (!activeTile) {
-//     activeTile = element;
-//     return;
-//   }
-
-//   if (isMatch(element)) {
-//     markTilesAsRevealed(element);
-//     checkWinCondition();
-//   } else {
-//     hideTiles(element);
-//   }
-// }
-
-// function isMatch(tile) {
-//   return (
-//     tile.getAttribute("data-color") === activeTile.getAttribute("data-color")
-//   );
-// }
-
-// function markTilesAsRevealed(tile) {
-//   activeTile.setAttribute("data-revealed", "true");
-//   tile.setAttribute("data-revealed", "true");
-//   activeTile = null;
-//   awaitingEndOfMove = false;
-//   revealedCount += 2;
-// }
-
-// function checkWinCondition() {
-//   if (revealedCount === tileCount) {
-//     alert("You win!");
-//   }
-// }
-
-// function hideTiles(tile) {
-//   awaitingEndOfMove = true;
-//   setTimeout(() => {
-//     tile.style.backgroundColor = null;
-//     activeTile.style.backgroundColor = null;
-
-//     awaitingEndOfMove = false;
-//     activeTile = null;
-//   }, 1000);
-// }
-
-// function shuffle(array) {
-//   for (let i = array.length - 1; i > 0; i--) {
-//     let j = Math.floor(Math.random() * (i + 1));
-//     [array[i], array[j]] = [array[j], array[i]];
-//   }
-//   return array;
-// }
-
-// function buildTile(color) {
-//   const element = document.createElement("div");
-
-//   element.classList.add("tile");
-//   element.tabIndex = "0";
-//   element.setAttribute("data-color", color);
-//   element.setAttribute("data-revealed", "false");
-
-//   element.addEventListener("click", () => handleTileClick(element, color));
-
-//   return element;
-// }
